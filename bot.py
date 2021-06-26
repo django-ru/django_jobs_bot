64c9e7fedebcc0ccf2e6d3a4ce67020063586f43
Rustem Saiargaliev <rustem.saiargaliev@voiio.de>
2021-06-26 16:12:52 +0200

    Add warn command

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ import os
 from functools import reduce
 
 import sentry_sdk
-from telegram import Message, Update
-from telegram.ext import CallbackContext, MessageHandler, Updater
+from telegram import Message, ParseMode, Update
+from telegram.ext import CallbackContext, CommandHandler, MessageHandler, Updater
 from telegram.ext.filters import MessageFilter, Filters
 
 logging.basicConfig(
@@ -21,21 +21,30 @@ def forward(update: Update, context: CallbackContext):
     update.message.forward(chat_id=chat_id_to_forward)
 
 
+def warn(update: Update, context: CallbackContext):
+    rules_url = os.getenv("RULES_URL")
+    message = f"Привет! У нас есть [правила оформления вакансий и резюме]({rules_url}). " \
+              f"Отредактируйте ваше сообщение и оно будет отправлено в канал @django\\_jobs\\_board."
+    update.message.reply_to_message.reply_text(
+        text=message, parse_mode=ParseMode.MARKDOWN, quote=True
+    )
+    update.message.delete()
+
+
 def log_errors(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 
 class ContainsJobHashTag(MessageFilter):
-    JOB_HASHTAGS = ["#cv", "#job", "#вакансия", "#работа"]
+    JOB_HASHTAGS = ["#cv", "#job"]
 
     def filter(self, message: Message) -> bool:
         return any([tag in message.text.lower() for tag in self.JOB_HASHTAGS])
 
 
 def with_default_filters(*filters):
-    """Apply default filters to the given filter classes"""
+    """Apply default filters to the given filter classes."""
     default_filters = [
-        Filters.text,
         Filters.chat(
             chat_id=int(os.getenv("CHAT_ID_FROM")),
         ),
@@ -45,6 +54,12 @@ def with_default_filters(*filters):
 
 def forward_messages_that_match(*filters) -> MessageHandler:
     return MessageHandler(filters=with_default_filters(*filters), callback=forward)
+
+
+def reply_warning_to_messages_that_match(*filters) -> CommandHandler:
+    return CommandHandler(
+        command="warn", filters=with_default_filters(*filters), callback=warn
+    )
 
 
 def in_heroku() -> bool:
@@ -60,9 +75,21 @@ def init_sentry():
 
 def main():
     bot_token = os.getenv("BOT_TOKEN")
+    admins = os.getenv("ADMINS").split(",")
 
     bot = Updater(bot_token)
-    bot.dispatcher.add_handler(forward_messages_that_match(ContainsJobHashTag))
+    bot.dispatcher.add_handler(
+        forward_messages_that_match(
+            ContainsJobHashTag,
+            Filters.text,
+            ~Filters.command,
+        )
+    )
+    bot.dispatcher.add_handler(
+        reply_warning_to_messages_that_match(
+            Filters.reply, Filters.command, Filters.user(username=admins)
+        )
+    )
     bot.dispatcher.add_error_handler(log_errors)
 
     if in_heroku():
@@ -70,7 +97,7 @@ def main():
         init_sentry()
         bot.start_webhook(
             listen="0.0.0.0",
-            port=int(os.getenv("PORT", 5000)),
+            port=int(os.getenv("PORT")),
             url_path=bot_token,
             webhook_url=f"https://{app_name}.herokuapp.com/" + bot_token,
         )

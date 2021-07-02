4111904414d99a8e231736aaf25ec0ed8257da06
Rust Saiargaliev <rustem.saiargaliev@voiio.de>
2021-07-02 12:22:06 +0200

    Rewrite to work on Heroku (#1)
    * Rewrite to work on Heroku
    
    * Add warn command
    
    * Fix error logger
    
    * Fix hashtag filter in handler

--- bot.py
+++ bot.py
@@ -1,71 +1,110 @@
 import logging
+import operator
 import os
+from functools import reduce
 
-from telegram.ext import Updater, MessageHandler, Filters
+import sentry_sdk
+from telegram import Message, ParseMode, Update
+from telegram.ext import CallbackContext, CommandHandler, MessageHandler, Updater
+from telegram.ext.filters import MessageFilter, Filters
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-FORWARD_FROM_CHAT_ID = os.environ.get('FORWARD_FROM_CHAT_ID')
-FORWARD_TO_CHAT_ID = os.environ.get('FORWARD_TO_CHAT_ID')
-CREATOR_ID = os.environ.get('CREATOR_ID')
-BOT_POLL_INTERVAL = os.environ.get('BOT_POLL_INTERVAL', 3600)
-TAGS_TO_FORWARD = ['#cv', '#job', '#вакансия', '#работа']
 
-SOCKS_URL = os.environ.get('SOCKS_URL')
-SOCKS_USER = os.environ.get('SOCKS_USER')
-SOCKS_PASSWORD = os.environ.get('SOCKS_PASSWORD')
+def forward(update: Update, context: CallbackContext):
+    chat_id_to_forward = int(os.getenv("CHAT_ID_TO"))
+    logger.info("Forwarding message to %s", chat_id_to_forward)
+    update.message.forward(chat_id=chat_id_to_forward)
 
 
-def forward_message(bot, update):
-    if str(update.message.chat.id) != FORWARD_FROM_CHAT_ID:
-        logger.warning('Blocked attempt to forward from %s', update.message.chat.id)
-        return
-    if any([tag in update.message.text.lower() for tag in TAGS_TO_FORWARD]):
-        forward_to = FORWARD_TO_CHAT_ID
-    else:
-        forward_to = CREATOR_ID
-
-    logger.warning('Forwarding message to %s', forward_to)
-    bot.forward_message(
-        chat_id=forward_to,
-        from_chat_id=update.message.chat.id,
-        message_id=update.message.message_id
+def warn(update: Update, context: CallbackContext):
+    rules_url = os.getenv("RULES_URL")
+    message = f"Привет! У нас есть [правила оформления вакансий и резюме]({rules_url}). " \
+              f"Отредактируйте ваше сообщение и оно будет отправлено в канал @django\\_jobs\\_board."
+    update.message.reply_to_message.reply_text(
+        text=message, parse_mode=ParseMode.MARKDOWN, quote=True
     )
+    update.message.delete()
 
 
-def error(bot, update, error):
+def log_errors(update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 
-def start_bot():
-    if not all([BOT_TOKEN, FORWARD_FROM_CHAT_ID, FORWARD_TO_CHAT_ID, CREATOR_ID]):
-        raise ValueError(
-            'One of the variables -'
-            'BOT_TOKEN, FORWARD_FROM_CHAT_ID, FORWARD_TO_CHAT_ID or CREATOR_ID'
-            'is missing in environment'
+class ContainsJobHashTag(MessageFilter):
+    JOB_HASHTAGS = ["#cv", "#job"]
+
+    def filter(self, message: Message) -> bool:
+        return any([tag in message.text.lower() for tag in self.JOB_HASHTAGS])
+
+
+def with_default_filters(*filters):
+    """Apply default filters to the given filter classes."""
+    default_filters = [
+        Filters.chat(
+            chat_id=int(os.getenv("CHAT_ID_FROM")),
+        ),
+    ]
+    return reduce(operator.and_, [*default_filters, *filters])
+
+
+def forward_messages_that_match(*filters) -> MessageHandler:
+    return MessageHandler(filters=with_default_filters(*filters), callback=forward)
+
+
+def reply_warning_to_messages_that_match(*filters) -> CommandHandler:
+    return CommandHandler(
+        command="warn", filters=with_default_filters(*filters), callback=warn
+    )
+
+
+def in_heroku() -> bool:
+    return os.getenv("HEROKU_APP_NAME") is not None
+
+
+def init_sentry():
+    sentry_dsn = os.getenv("SENTRY_DSN")
+
+    if sentry_dsn:
+        sentry_sdk.init(sentry_dsn)
+
+
+def main():
+    bot_token = os.getenv("BOT_TOKEN")
+    admins = os.getenv("ADMINS").split(",")
+
+    bot = Updater(bot_token)
+    bot.dispatcher.add_handler(
+        forward_messages_that_match(
+            ContainsJobHashTag(),
+            Filters.text,
+            ~Filters.command,
         )
+    )
+    bot.dispatcher.add_handler(
+        reply_warning_to_messages_that_match(
+            Filters.reply, Filters.command, Filters.user(username=admins)
+        )
+    )
+    bot.dispatcher.add_error_handler(log_errors)
 
-    if SOCKS_URL:
-        proxy_config = {
-            'proxy_url': SOCKS_URL,
-            'urllib3_proxy_kwargs': {'username': SOCKS_USER, 'password': SOCKS_PASSWORD}
-        }
+    if in_heroku():
+        app_name = os.getenv("HEROKU_APP_NAME")
+        init_sentry()
+        bot.start_webhook(
+            listen="0.0.0.0",
+            port=int(os.getenv("PORT")),
+            url_path=bot_token,
+            webhook_url=f"https://{app_name}.herokuapp.com/" + bot_token,
+        )
+        bot.idle()
     else:
-        proxy_config = None
-    updater = Updater(BOT_TOKEN, request_kwargs=proxy_config)
-
-    dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.text, forward_message))
-    dp.add_error_handler(error)
-
-    logger.info('Start polling...')
-    updater.start_polling(poll_interval=float(BOT_POLL_INTERVAL))
-    updater.idle()
+        bot.start_polling()
 
 
-if __name__ == '__main__':
-    start_bot()
+if __name__ == "__main__":
+    main()
